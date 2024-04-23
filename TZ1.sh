98de2f800c1d5fb5ee4d1ad9cb582115bc75f73d
smallbebrych <glebodrov2005phantom@gmail.com>
2024-04-23 13:27:06 +0300

    Task 1

---
+++ TZ1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+find "$1" -type f -exec sh -c '
+for file; do
+base=$(basename "$file")
+ext="${base##*.}"
+name="${base%.*}"
+numb=""
+count=0
+while [ -e "$2/$name$numb.$ext" ]; do
+count=$((count + 1))
+numb="$count"
+done
+cp "$file" "$2/$name$numb.$ext"
+done
+' sh {} "$2" \;
